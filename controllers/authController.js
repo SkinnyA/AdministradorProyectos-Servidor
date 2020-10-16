23e2e34ecac32e1b2710424a70d2a380dee343ed
unknown <aarguelles05@gmail.com>
2020-10-16 17:31:14 +0200

    Adding validation when login with an user

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,4 +9,41 @@ exports.autenticarUsuario = async (req, res) => {
     if(!errores.isEmpty()){
         return res.status(400).json({errores: errores.array()});
     }
+
+    // extraer el email y password
+    const {email, password} = req.body;
+
+    try {
+        // revisar que sea un usuario registrado
+        let usuario = await Usuario.findOne({email});
+        if(!usuario){
+            return res.status(400).json({msg: 'El usuario no existe'});
+        }
+
+        // revisar el password
+        const passCorrecto = await bcryptjs.compare(password, usuario.password);
+        if(!passCorrecto){
+            return res.status(400).json({msg: 'Password Incorrecto'});
+        }
+
+        // si todo es correcto crear y firmar el jwt
+        const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+        // firmar el jwt
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 // 1 hora
+        }, (error, token) => {
+            if(error) throw error;
+
+            // mensaje de confirmacion
+            res.json({token: token});
+
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
