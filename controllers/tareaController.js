e38bb9338252105278d88cb317e00ec37357b94c
unknown <aarguelles05@gmail.com>
2020-10-18 23:53:18 +0200

    Update tasks

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -63,3 +63,40 @@ exports.obtenerTareas = async(req,res) => {
         res.status(500).send('Hubo un error');
     }
 }
+
+// actualizar tarea
+exports.actualizarTarea = async(req,res) => {
+    try {
+        // extraer el proyecto y comprobar si existe
+        const {proyecto, nombre, estado} = req.body;
+
+        // si la tarea existe
+        let tarea = await Tarea.findById(req.params.id);
+
+        if(!tarea){
+            return res.status(404).json({msg: 'No existe esa tarea'});
+        }
+
+        // extraer proyecto
+        const exiteProyecto = await Proyecto.findById(proyecto);
+
+        // revisar si el proyecto actual pertenece al usuario autenticado
+        if(exiteProyecto.creador.toString() !== req.usuario.id){
+            return res.status(401).json({msg: 'No autorizado'});
+        }
+
+        // crear un objeto con la nueva informacion
+        const nuevaTarea = {};
+
+        if(nombre) nuevaTarea.nombre = nombre;
+        if(estado) nuevaTarea.estado = estado;
+
+        // guardar la tarea
+        tarea = await Tarea.findOneAndUpdate({_id: req.params.id}, nuevaTarea, {new: true});
+        res.json({tarea});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
--- routes/tareas.js
+++ routes/tareas.js
@@ -21,4 +21,10 @@ router.get('/',
     tareaController.obtenerTareas
 );
 
+// actualizar una tarea
+router.put('/:id', 
+    auth,
+    tareaController.actualizarTarea
+)
+
 module.exports = router;
